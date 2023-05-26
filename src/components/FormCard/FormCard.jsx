b229d4d959417f2075ac12e2d1853aee4074d05e
Loki99911 <nikita_kresik@i.ua>
2023-05-26 22:37:53 +0300

    add fix FormCard

--- src/components/FormCard/FormCard.jsx
+++ src/components/FormCard/FormCard.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import {
   FormCardInput,
   FormCardInputWrapper,
@@ -6,24 +6,41 @@ import {
   FormCardTitle,
   FormCardWrapper,
 } from './FormCard.styled';
+import { MealsContext } from 'components/App';
+import shortid from 'shortid';
 
 export const FormCard = ({ prop }) => {
   const { name, photo, price } = prop;
-  const [value, setValue] = useState(0);
+  const { meals, setMeals } = useContext(MealsContext);
+  // const [value, setValue] = useState(0);
+  const cardID = shortid.generate();
 
-  const handleChange = event => {
-    setValue(event.target.value);
-  };
+  const baseValue = meals.find(meal => (meal.id === cardID ? meal.value : 0));
+  const [value, setValue] = useState(baseValue || 0);
+
+
+const handleChange = event => {
+  const newValue = event.target.value;
+  setValue(newValue);
+  const updatedMeals = meals.map(meal => {
+    if (meal.id === cardID) {
+      return { ...meal, value: newValue };
+    }
+    return meal;
+  });
+  console.log(updatedMeals);
+  setMeals(updatedMeals);
+};
+
   return (
     <FormCardWrapper>
       <FormCardImg src={`${photo}`} alt={`${name}`} />
-
       <FormCardInputWrapper>
         <FormCardTitle>{name}</FormCardTitle>
         <FormCardTitle>{price}</FormCardTitle>
         <FormCardInput
           type="number"
-          id="numberInput"
+          id={cardID}
           value={value}
           onChange={handleChange}
           step="1"
